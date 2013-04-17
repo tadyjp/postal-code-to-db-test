89259c5b928550dd4fd832c1da48a7f9b34d6c01
tady <a.dat.jp@gmail.com>
2013-04-18 02:13:33 +0900

    init

---
+++ main.rb
@@ -0,0 +1,12 @@
+
+require 'mysql2'
+
+
+# mysql test
+
+# client = Mysql2::Client.new(:host => "localhost", :username => "tady", database: "postal_code_test")
+# results = client.query("SELECT * FROM users WHERE group='githubbers'")
+
+
+
+# mongo test
